493c91e19b51b348f96804a904082be59053fc3c
Lance Brignoni <lance@brignoni.space>
2016-01-22 22:38:45 -0800

    Update mysql_expire.py

--- mysql_expire.py
+++ mysql_expire.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Russian Roulette IRC bot
-# Created by Lance Brignoni on 10.5.15
+# Pomf Expirey Support
+# Created by Lance Brignoni on 1.20.16
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
